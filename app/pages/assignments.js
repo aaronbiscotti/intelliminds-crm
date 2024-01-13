0ad6a0b65db4313586d5564c3be6f53e55aa3d55
bedirbxsci <bedir@bxscience.edu>
2024-01-13 14:00:36 -0500

    assignmentschange

---
+++ app/pages/assignments.js
@@ -0,0 +1,12 @@
+// pages/new-assignments.js
+import React from 'react';
+
+export default function AssignmentsPage() {
+    return (
+        <div className="container">
+            {/* Add the HTML and CSS that would replicate the look of the page you want based on the screenshot */}
+            <h1>New Assignments</h1>
+            {/* Rest of your page content */}
+        </div>
+    );
+}
